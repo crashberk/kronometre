ff16d923c039e37b57e52a3dc3784822132a2991
crashberk <gece_kartali5@hotmail.com>
2017-08-14 15:12:11 +0300

    Browserify kuruldu ve de geliştirmemiz commonjs modül yapısına uygun hale getirildi

--- index.js
+++ index.js
@@ -1,39 +1,7 @@
-// global zaman aşımı numarası
-var zamanAsimiNosu;
 
-/**
-* Parametre olarak verilen sayaç değerini yine parametre olarak
-* verilen HTML elemanina yazar. Her saniye kendi çağrıp 
-* değeri azaltır
-* 
-* @param {HTMLElement} sayacEl - Değerin yazılacağı eleman
-* @param {Number }sayacDegeri - Sayacin o anki değeri
-* 
-* @returns
-*/
-function geriSaydir(sayacEl, sayacDegeri) {
-    // sayac degerini her saniye azalt ve göster
-    sayacEl.innerHTML = sayacDegeri;
-
-    zamanAsimiNosu = window.setTimeout(function () {
-        if (sayacDegeri > 0) {
-            geriSaydir(sayacEl, sayacDegeri - 1);
-        }
-
-    }, 1000);
-}
-
-function durdur(timeoutID) {
-    window.clearTimeout(timeoutID);
-}
-
-function sifirla(sayacEl, timeoutID) {
-    window.clearTimeout(timeoutID);
-    sayacEl.innerHTML = "";
-    zamanAsimiNosu = null;
-}
-
-
+// Araçlar modülümü import et
+// gerekli metodlarına böylelikle ulaşabiliriz
+var araclar = require('./araclar');
 
 window.addEventListener("load", function (event) {
 
@@ -50,22 +18,22 @@ window.addEventListener("load", function (event) {
     butonEl.addEventListener('click', function (event) {
 
         var sayacDegeri = Number(sayacBaslangiciEl.value);
-        if (typeof zamanAsimiNosu !== 'undefined' && zamanAsimiNosu !== null) {
+        if (typeof araclar.zamanAsimiDegeriGoster() !== 'undefined' && araclar.zamanAsimiDegeriGoster() !== null) {
             sayacDegeri = Number(sayacEl.innerHTML);
         }
 
         // sayac değerine başlangıç değerini ata
-        geriSaydir(sayacEl, sayacDegeri);
+        araclar.geriSaydir(sayacEl, sayacDegeri);
 
     });
 
 
     durdurEl.addEventListener('click', function (event) {
-        durdur(zamanAsimiNosu);
+        araclar.durdur();
     });
 
     sifirlaEl.addEventListener('click', function (event) {
-        sifirla(sayacEl, zamanAsimiNosu);
+        araclar.sifirla(sayacEl);
     });
 
 });
